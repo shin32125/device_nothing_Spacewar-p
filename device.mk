cc3985507648967f90c01e3fe20be29e7e4b8237
shin32125 <shintaga32123@gmail.com>
2023-06-22 21:09:51 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -366,7 +366,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service.pn8x \
+    android.hardware.nfc@1.2-service \
     NfcNci \
     SecureElement \
     Tag
