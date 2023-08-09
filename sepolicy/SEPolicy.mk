30ca430cfe5cce84e2fb912c58404a6b7faf85e2
shin32125 <shintaga32123@gmail.com>
2023-08-09 23:53:11 +0900

    Revert "post"
    This reverts commit 6d071a328fefcf374d18aa348f0222905c780ad0.

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -5,4 +5,5 @@
 #
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/public
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/private
