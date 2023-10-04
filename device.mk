f068385f7d93dc7f2a481406633dfbd2e33288f3
shin32125 <shintaga32123@gmail.com>
2023-10-04 17:09:53 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -403,7 +403,8 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
+    libpsi.vendor \
+    libtflite
 
 # IRQ balance config
 PRODUCT_COPY_FILES += \
