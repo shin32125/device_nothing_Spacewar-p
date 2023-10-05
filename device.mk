47be14cfddfb98f5c3c6c79655eb90589927a8ae
shin32125 <shintaga32123@gmail.com>
2023-10-05 18:09:56 +0900

    post

--- device.mk
+++ device.mk
@@ -225,8 +225,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+# Native libraries whitelist
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.system_ext.txt:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/public.libraries.txt
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Display
 PRODUCT_PACKAGES += \
