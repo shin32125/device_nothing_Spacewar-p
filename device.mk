73f83955c329379b43fcc78e88640a30f1199166
shin32125 <shintaga32123@gmail.com>
2023-10-06 07:07:13 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -48,8 +48,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/nothing \
-    hardware/google/interfaces \
-    hardware/google/pixel \
     vendor/qcom/opensource/usb/etc
 
 # A/B
