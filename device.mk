03c2e35c2faef95e30be1143fff5775c5788a44a
shin32125 <shintaga32123@gmail.com>
2023-10-06 20:40:14 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -462,7 +462,6 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    android.hardware.sensors@2.0-ScopedWakelock \
     libsensorndkbridge \
     sensors.nothing
 
