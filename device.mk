6d5a47ec3655d4088d64135375a16f8b621c0f01
shin32125 <shintaga32123@gmail.com>
2023-10-07 10:14:19 +0900

    Revert "Spacewar: Switch BtAudio to AIDL"

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
