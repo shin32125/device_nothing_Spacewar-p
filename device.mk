1595ca2a3ecf64bd36f53b41054615ec244206e2
shin32125 <shintaga32123@gmail.com>
2023-10-17 18:07:53 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -48,7 +48,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     vendor/nxp/nfc \
-    SecureElement \
     hardware/nothing \
     hardware/google/interfaces \
     hardware/google/pixel \
