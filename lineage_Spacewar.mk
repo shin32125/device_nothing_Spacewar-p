2c84912b25c5c30b35f4fc53634e1c4651e5363a
shin32125 <shintaga32123@gmail.com>
2023-11-26 02:08:35 +0900

    Update lineage_Spacewar.mk

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -11,9 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from the device configuration.
 $(call inherit-product, device/nothing/Spacewar/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-EVO_BUILD_TYPE := COMMUNITY
+# Inherit from the crdroid configuration.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -25,7 +24,7 @@ TARGET_USES_PICO_GAPPS := true
 TARGET_USES_NOTHING_CAMERA := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_Spacewar
+PRODUCT_NAME := lineage_Spacewar
 PRODUCT_DEVICE := Spacewar
 PRODUCT_BRAND := Nothing
 PRODUCT_MODEL := A063
