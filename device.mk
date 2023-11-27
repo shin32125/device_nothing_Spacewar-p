18b7c9afdd0bba16a0f860ada3b9c0a37ded977b
shin32125 <shintaga32123@gmail.com>
2023-11-27 19:22:17 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,7 +27,8 @@ $(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # RRO (evo)
 PRODUCT_PACKAGES += \
