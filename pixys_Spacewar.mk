d0414623a330b54f3034802f50ead0600d0e1f2c
userariii <abhattacharjee717@gmail.com>
2024-01-12 21:05:09 +0000

    Spacewar: Bringup Dolby with Spacial audio
    * Replaces Nothing Dirac and Google Spacial audio
      Revert and fixup of c531714cf32cc5868648f01af19bcb41576db43c
    
    Co-authored-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>

--- pixys_Spacewar.mk
+++ pixys_Spacewar.mk
@@ -33,6 +33,7 @@ TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_INCLUDE_OEM_CAMERA := true
+TARGET_SHIPS_CUSTOM_DOLBY := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := pixys_Spacewar
