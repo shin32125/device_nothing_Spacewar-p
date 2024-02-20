zelleb/device_nothing_Spacewar

[TMP] Revert "Spacewar: Implement OnePlus Dolby"
This reverts commit 11774d9e28265258b1070cc272fbb356ca98c2ca.

reason-for-revert:  Need to investigate further about spatial audio in apple music

Signed-off-by: aswin7469 <aswinas@pixysos.com>

BoardConfig.mk
@@ -119,8 +119,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nothing
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/pixys/config/device_framework_matrix.xml \
-    hardware/dolby/dolby_framework_matrix.xml
+    vendor/pixys/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest_yupik.xml
 

shin32125/device_nothing_Spacewar-p

BoardConfig.mk
@@ -122,8 +122,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.nothing
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/evolution/config/device_framework_matrix.xml \
-    hardware/dolby/dolby_framework_matrix.xml
+    vendor/evolution/config/device_framework_matrix.xml
+
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest_yupik.xml
 