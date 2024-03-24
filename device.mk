0857acbc95b5d18bf3d918da244d3af8eb41c204
shin32125 <shintaga32123@gmail.com>
2024-03-24 22:31:12 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -134,6 +134,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
+    audio.primary.lahaina \
     audio.r_submix.default \
     audio.usb.default \
     audioadsprpcd \
