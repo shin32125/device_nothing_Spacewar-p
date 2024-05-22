b41f5446df9aa6adec64dbc7cc560d523eb084ef
shin32125 <shintaga32123@gmail.com>
2024-05-23 00:22:37 +0900

    p

--- evolution_Spacewar.mk
+++ evolution_Spacewar.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/nothing/Spacewar/device.mk)
 
 # Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 EVO_BUILD_TYPE := COMMUNITY
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
