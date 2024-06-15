810bf5af29b9cca86227159039991b4a9461f1e6
shin32125 <shintaga32123@gmail.com>
2024-06-15 14:53:08 +0900

    Update lineage_Spacewar.mk

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_ENABLE_BLUR := true
-TARGET_GAPPS_ARCH  := arm64
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Matrixx
