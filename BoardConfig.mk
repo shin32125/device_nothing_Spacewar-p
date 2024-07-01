927fcceb9ff30d4fc0bdf8cb3a4b2fe694c9a908
shin32125 <shintaga32123@gmail.com>
2024-07-01 14:50:17 +0900

    Revert "p"
    This reverts commit e615e66c1eccb25ae11cd1c22d8532a02b3da893.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,9 +154,6 @@ TARGET_COPY_OUT_PRODUCT := product
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 
-# PowerShare
-TARGET_POWERSHARE_NODE := /sys/class/qcom-battery/wireless_boost_en
-
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
 
