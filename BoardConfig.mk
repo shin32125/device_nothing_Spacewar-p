c71a1898a3d3e1b5fd82ca275ca50fc4ce7d9503
shin32125 <shintaga32123@gmail.com>
2024-07-04 18:31:54 +0900

    Revert "Update BoardConfig.mk"
    This reverts commit ec2a2aa2b1eef99493aea7038386367fa4cdb81c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -220,5 +220,3 @@ CONFIG_IEEE80211AX := true
 
 # Include the proprietary files BoardConfig.
 include vendor/nothing/Spacewar/BoardConfigVendor.mk
-include vendor/lineage/config/BoardConfigLineage.mk
-
