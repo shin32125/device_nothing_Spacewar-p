c53c66e028c497fc686c60623e34fb18ad887644
shin32125 <shintaga32123@gmail.com>
2024-09-15 14:41:34 +0900

    p

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -14,6 +14,8 @@ $(call inherit-product, device/nothing/Spacewar/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_USES_PICO_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_Spacewar
 PRODUCT_DEVICE := Spacewar
