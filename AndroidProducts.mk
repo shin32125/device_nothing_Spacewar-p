650fa4d207714d2c5c30b5c09cb9320eeedd4370
shin32125 <shintaga32123@gmail.com>
2024-09-16 00:48:09 +0900

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_Spacewar.mk
+    $(LOCAL_DIR)/lineage_Spacewar.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_Spacewar-eng \
-    evolution_Spacewar-userdebug \
-    evolution_Spacewar-user
+    lineage_Spacewar-eng \
+    lineage_Spacewar-userdebug \
+    lineage_Spacewar-user
