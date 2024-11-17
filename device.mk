f301719f1c3158ec5a379df00bd1bd03639fe2ca
shin32125 <shintaga32123@gmail.com>
2024-11-17 19:52:58 +0900

    Update device.mk

--- device.mk
+++ device.mk
@@ -532,7 +532,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb-service.qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
