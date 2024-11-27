91b2bc654677955b84edc2e3399d4910525ceb21
shin32125 <shintaga32123@gmail.com>
2024-11-27 15:46:08 +0900

    p

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2021 PixysOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/bliss_Spacewar.mk
+
+COMMON_LUNCH_CHOICES := \
+    bliss_Spacewar-eng \
+    bliss_Spacewar-userdebug \
+    bliss_Spacewar-user
