4b02ca2721ef2e78f4ff65d646b4174ec9232be7
shin32125 <shintaga32123@gmail.com>
2024-11-27 19:43:02 +0900

    Performance tuning

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-8192-dalvik-heap.mk)
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
