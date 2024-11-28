3ce0dc05e0c7fff7dcf11b2499a9708d7a922d78
shin32125 <shintaga32123@gmail.com>
2024-11-28 19:18:51 +0900

    p

--- device.mk
+++ device.mk
@@ -465,6 +465,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.voice-V1-ndk.vendor \
     android.hardware.radio-V1-ndk.vendor
 
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
