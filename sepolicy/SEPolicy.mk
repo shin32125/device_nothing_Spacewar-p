183dcc80999988dc662b74398363ccdeff02e722
shin32125 <shintaga32123@gmail.com>
2024-12-23 18:10:23 +0900

    d

--- sepolicy/SEPolicy.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2018-2021 Evolution-X
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-BOARD_VENDOR_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/vendor
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/public
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/nothing/Spacewar/sepolicy/private
